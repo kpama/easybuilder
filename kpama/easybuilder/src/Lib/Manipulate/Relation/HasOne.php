3f21f27e97a3ecc6f4a047fb5a5708c08b49fa13
Jibao Mansaray <jibaomansaray@gmailcom>
2021-06-04 14:10:49 +0000

    Add HasOne and Relation manipulator classes

--- kpama/easybuilder/src/Lib/Manipulate/Relation/HasOne.php
+++ kpama/easybuilder/src/Lib/Manipulate/Relation/HasOne.php
@@ -5,16 +5,38 @@ namespace Kpama\Easybuilder\Lib\Manipulate\Relation;
 class HasOne extends Relation
 {
 
-    public function apply(bool $remove = false)
+    public function apply(bool $remove = false): object
     {
-        dd($this->definition);
 
-        $model = $this->entity->createOrUpdateWithDefinition(
-            $this->definition['definition'],
-            $this->definition['definition']['class'],
-            $this->data
-        );
-        dump($this->definition);
-        dd('we are in "has one" relation class');
+        $idField = $this->entity->getEntityIdField($this->definition);
+        $isNew = false;
+
+        if (!isset($this->data[$idField])) {
+            $isNew = true;
+            $value = $this->model->{$this->definition['local_key']};
+            $this->data[$this->definition['foreign_key']] = $value;
+
+            $model = $this->entity->createOrUpdateWithDefinition(
+                $this->definition,
+                $this->definition['class'],
+                $this->data
+            );
+        }
+
+        if(!$isNew) {
+            $model = $this->entity->createOrUpdateWithDefinition(
+                $this->definition,
+                $this->definition['class'],
+                $this->data
+            );
+        }
+
+        if($remove) {
+            $this->model->{$this->relation}()->delete();
+        } else {
+            $this->model->{$this->relation}()->save($model);
+        }
+
+        return $this->model;
     }
 }
--- kpama/easybuilder/src/Lib/Manipulate/Relation/Relation.php
+++ kpama/easybuilder/src/Lib/Manipulate/Relation/Relation.php
@@ -4,22 +4,32 @@ namespace Kpama\Easybuilder\Lib\Manipulate\Relation;
 
 use Kpama\Easybuilder\Lib\Manipulate\Entity;
 
-abstract class Relation {
+abstract class Relation
+{
 
     protected $model;
     protected array $definition;
+    protected array $fullDefinition;
     protected Entity $enity;
     protected array $data;
+    protected string $relation;
 
 
-    public function __construct(object $model, array $definition, array $data)
-    {
-       $this->model  = $model; 
-       $this->definition = $definition;
-       $this->entity = new Entity();
-       $this->data = $data;
+    public function __construct(
+        object $model,
+        array $definition,
+        array $data,
+        string $relation,
+        array $fullDefinition
+    ) {
+        $this->model  = $model;
+        $this->definition = $definition;
+        $this->entity = new Entity();
+        $this->data = $data;
+        $this->relation = $relation;
+        $this->fullDefinition = $fullDefinition;
     }
 
 
-    public abstract function apply(bool $remove = false);
+    public abstract function apply(bool $remove = false): object;
 }
