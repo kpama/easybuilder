eb25213d30796a4a7362b15faa69aaa4260f186e
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-03 15:25:24 +0000

    Move swagger assets to the public folder

--- kpama/easybuilder/webpack.mix.js
+++ kpama/easybuilder/webpack.mix.js
@@ -12,4 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('src/resources/js/kpamaeasybuilder.js', 'src/public/js')
+mix.js('src/resources/js/swagger.js', 'src/public/js')
     .sass('src/resources/sass/kpamaeasybuilder.scss', 'src/public/css');
+mix.copy('node_modules/swagger-ui/dist/swagger-ui.css', 'src/public/css');
+mix.copy('node_modules/swagger-ui/dist/swagger-ui.css.map', 'src/public/css');
