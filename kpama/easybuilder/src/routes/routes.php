0ed63dd63d88be4dd5a0793cf35b492a3fb3af3f
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-03 15:24:28 +0000

    Start work on generating the urls for swagger

--- kpama/easybuilder/src/routes/routes.php
+++ kpama/easybuilder/src/routes/routes.php
@@ -7,9 +7,26 @@ Route::group(['middleware' => ['web']], function () {
 
    // Add your web  routes here
    Route::get('kpamaeasybuilder','Kpama\Easybuilder\Controllers\HelloController@indexAction');
-   Route::get('test', [HelloController::class, 'testAction']);
-   Route::get('apitest', [HelloController::class, 'apiTestAction']);
+   Route::get('kpamaeasybuilder/swagger', [HelloController::class, 'swaggerAction']);
+   Route::get('kpamaeasybuilder/swagger-api/{resource}', [HelloController::class, 'swaggerApiAction']);
+   Route::get('kpamaeasybuilder/swagger-config', function () {
+	   $links = [];
+	   collect(Route::getRoutes())->each(function($route) use(&$links){
+		   if(isset($route->defaults['_easy_generated']) && isset($route->defaults['resource']) ) {
+			$resource = $route->defaults['resource'];
+			$links[$resource] = [
+				'url' => '/kpamaeasybuilder/swagger-api/' . $resource,
+				'name' => str_replace('-', ' ', $resource)
+		       ];
+		   } 
+	   
+	   });
 
+
+	   return [
+		   'urls' =>  array_values($links)
+	   ];
+   });
 });
 
 
