bfac392cdca35079d8889aab04220fd2afb12c58
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-04 15:18:10 +0000

    Code formatting

--- kpama/easybuilder/src/routes/routes.php
+++ kpama/easybuilder/src/routes/routes.php
@@ -6,18 +6,21 @@ use Kpama\Easybuilder\Controllers\SwaggerController;
 
 Route::group(['middleware' => ['web']], function () {
 
-   // Add your web  routes here
-    // @todo Check configuration before registering these routes
-    Route::get('kpamaeasybuilder', 'Kpama\Easybuilder\Controllers\HelloController@indexAction');
-    Route::get('kpamaeasybuilder/swagger', [SwaggerController::class, 'swaggerAction']);
-    Route::get('kpamaeasybuilder/swagger-api/{resource}', [SwaggerController::class, 'swaggerApiAction']);
-    Route::get('kpamaeasybuilder/swagger-config', [SwaggerController::class, 'configAction']);
+	// Add your web  routes here
+	Route::get('kpamaeasybuilder', 'Kpama\Easybuilder\Controllers\HelloController@indexAction');
 
+	// swagger routes
+	if (config('kpamaeasybuilder.register_sawgger_route')) {
+		$prefix = config('kpamaeasybuilder.swagger_uri_prefix');
+		Route::get("{$prefix}/swagger", [SwaggerController::class, 'swaggerAction']);
+		Route::get("{$prefix}/swagger-api/{resource}", [SwaggerController::class, 'swaggerApiAction']);
+		Route::get("{$prefix}/swagger-config", [SwaggerController::class, 'configAction']);
+	}
 
-    if (config('env') != 'production') {
-        /**
-         * Make sure to publish your public assets when going live
-         */
-        Route::get('kpamaeasybuilder/{path?}', 'Kpama\Easybuilder\Controllers\AssetController@serveAction')->where('path', '(.*)');
-    }
+	if (config('env') != 'production') {
+		/**
+		 * Make sure to publish your public assets when going live
+		 */
+		Route::get('kpamaeasybuilder/{path?}', 'Kpama\Easybuilder\Controllers\AssetController@serveAction')->where('path', '(.*)');
+	}
 });
