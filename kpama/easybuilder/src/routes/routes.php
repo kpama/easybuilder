4843b1cfdf60093baf056d994f3f364be4ba942f
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-04 15:04:18 +0000

    Move swagger actions to SwaggerController

--- kpama/easybuilder/src/routes/routes.php
+++ kpama/easybuilder/src/routes/routes.php
@@ -2,38 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use Kpama\Easybuilder\Controllers\HelloController;
+use Kpama\Easybuilder\Controllers\SwaggerController;
 
 Route::group(['middleware' => ['web']], function () {
 
    // Add your web  routes here
-   Route::get('kpamaeasybuilder','Kpama\Easybuilder\Controllers\HelloController@indexAction');
-   Route::get('kpamaeasybuilder/swagger', [HelloController::class, 'swaggerAction']);
-   Route::get('kpamaeasybuilder/swagger-api/{resource}', [HelloController::class, 'swaggerApiAction']);
-   Route::get('kpamaeasybuilder/swagger-config', function () {
-	   $links = [];
-	   collect(Route::getRoutes())->each(function($route) use(&$links){
-		   if(isset($route->defaults['_easy_generated']) && isset($route->defaults['resource']) ) {
-			$resource = $route->defaults['resource'];
-			$links[$resource] = [
-				'url' => '/kpamaeasybuilder/swagger-api/' . $resource,
-				'name' => str_replace('-', ' ', $resource)
-		       ];
-		   } 
-	   
-	   });
+    // @todo Check configuration before registering these routes
+    Route::get('kpamaeasybuilder', 'Kpama\Easybuilder\Controllers\HelloController@indexAction');
+    Route::get('kpamaeasybuilder/swagger', [SwaggerController::class, 'swaggerAction']);
+    Route::get('kpamaeasybuilder/swagger-api/{resource}', [SwaggerController::class, 'swaggerApiAction']);
+    Route::get('kpamaeasybuilder/swagger-config', [SwaggerController::class, 'configAction']);
 
 
-	   return [
-		   'urls' =>  array_values($links)
-	   ];
-   });
+    if (config('env') != 'production') {
+        /**
+         * Make sure to publish your public assets when going live
+         */
+        Route::get('kpamaeasybuilder/{path?}', 'Kpama\Easybuilder\Controllers\AssetController@serveAction')->where('path', '(.*)');
+    }
 });
-
-
-
-if (config('env') != 'production') {
-    /**
-     * Make sure to publish your public assets when going live
-     */
-    Route::get('kpamaeasybuilder/{path?}', 'Kpama\Easybuilder\Controllers\AssetController@serveAction')->where('path', '(.*)');
-}
