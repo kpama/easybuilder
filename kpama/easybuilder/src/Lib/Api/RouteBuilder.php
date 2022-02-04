b007c9f9f7aaafddde616dd66878dc56bed22f67
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-04 14:13:05 +0000

    Refactor the `generate` static method
    - We are now allow the user to handle each generated route
    - User can now override the default controller
    - Remove the method return type

--- kpama/easybuilder/src/Lib/Api/RouteBuilder.php
+++ kpama/easybuilder/src/Lib/Api/RouteBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Kpama\Easybuilder\Lib\Api;
 
+use Closure;
 use Illuminate\Routing\RouteCollection;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
@@ -10,36 +11,44 @@ use Kpama\Easybuilder\Lib\Parser;
 
 class RouteBuilder
 {
-    public static function generate(string $model, $authApi = true): RouteCollection
+    public static function generate(string|array $model, ?Closure $forEachRoute = null, ?string $controller = ApiController::class, string $slug = null)
     {
+        if(is_array($model)) {
+            foreach($model as $aModel) {
+                self::generate($aModel, $forEachRoute, $controller);
+            }
+            return;
+        }
+
         $parser = new Parser();
         $data = $parser->parse($model);
 
-        $slug = Str::slug($data['class']);
+        $slug = $slug ?: Str::slug($data['class']);
+        $controller = $controller ?: ApiController::class;
 
-        $routes = Route::apiResource($slug, ApiController::class)
+        $routes = Route::apiResource($slug, $controller)
             ->parameter($slug, 'id')
             ->register();
 
+
         // check if we can restore
         if ($data['can_soft_delete']) {
-            $routes->add(Route::match(['put', 'patch'], "{$slug}/restore/{id}", [ApiController::class, 'restore'])->name("{$slug}.restore"));
+            $routes->add(Route::match(['put', 'patch'], "{$slug}/restore/{id}", [$controller, 'restore'])->name("{$slug}.restore"));
         }
 
         if (!empty($data['scopes'])) {
-            $routes->add(Route::match(['get', 'head'], "{$slug}/scopes/{scope}", [ApiController::class, 'scope'])->name("{$slug}.scope"));
+            $routes->add(Route::match(['get', 'head'], "{$slug}/scopes/{scope}", [$controller, 'scope'])->name("{$slug}.scope"));
         }
 
         // @todo emit event to allow the user to add additional route
 
-        collect($routes)->each(function ($route) use ($data, $authApi) {
-            if($authApi) {
-                $route->middleware('auth:api');
-            }
+        collect($routes)->each(function ($route) use ($data, $forEachRoute) {
             $route->defaults('resource', $data['resource']);
             $route->defaults('_easy_generated', true);
-        });
 
-        return $routes;
+            if($forEachRoute) {
+                $forEachRoute($route);
+            }
+        });
     }
 }
