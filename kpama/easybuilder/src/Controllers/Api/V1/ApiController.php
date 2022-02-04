0e10a515842681df70e00cef161bb9ca6ff421a9
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-04 11:23:16 +0000

    Start calling the manipulator

--- kpama/easybuilder/src/Controllers/Api/V1/ApiController.php
+++ kpama/easybuilder/src/Controllers/Api/V1/ApiController.php
@@ -4,12 +4,13 @@ namespace Kpama\Easybuilder\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Kpama\Easybuilder\Lib\Manipulator;
 
 class ApiController extends BaseController
 {
   public function index(Request $request, string $resource)
   {
-    return ['resource' => $resource];
+    return (new Manipulator())->handleGetRequest($request, $resource);
   }
 
   public function scope(Request $request, string $scope, string $resource)
@@ -19,17 +20,17 @@ class ApiController extends BaseController
 
   public function store(Request $request, string $resource)
   {
-
+    return (new Manipulator())->handleCreateOrUpdateRequest($request, $resource);
   }
 
-  public function show(mixed $id, string $resource)
+  public function show(Request $request, mixed $id, string $resource)
   {
-
+    return (new Manipulator())->handleGetRequest($request, $resource, $id);
   }
 
   public function update(Request $request, mixed $id, string $resource)
   {
-
+    return (new Manipulator())->handleCreateOrUpdateRequest($request, $resource, $id);
   }
 
   public function destroy(mixed $id, string $resource)
