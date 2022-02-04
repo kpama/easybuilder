1c38cd60d9cdc432514fbc8728c3cfc02ec70e52
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-04 15:18:59 +0000

    Add configuration entries

--- kpama/easybuilder/src/config/config.php
+++ kpama/easybuilder/src/config/config.php
@@ -1,4 +1,25 @@
 <?php
+
+use Kpama\Easybuilder\Controllers\Api\V1\ApiController;
+
 return [
-    'version' => '0.0.1'
+    'version' => '0.0.1',
+
+    /*----------------------------------------------------------------
+     |  Allow the package to register routes for swagger documentation
+     |----------------------------------------------------------------
+    */
+    'register_sawgger_route' => env('REGISTER_SWAGGER_ROUTE', true),
+
+    /*----------------------------------------------------------------
+     |  Prefix to use for the swagger routes
+     |----------------------------------------------------------------
+    */
+    'swagger_uri_prefix' => env('SWAGGER_URI_PREFIX', 'kpamaeasybuilder'),
+
+    /*----------------------------------------------------------------
+     |  Default controller to use for API routes
+     |----------------------------------------------------------------
+    */
+    'api_controller' => ApiController::class
 ];
