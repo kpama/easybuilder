a8d701965162a7b3bf27ee854f789b1584aca73e
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-26 08:26:01 +0000

    Move the filter processing here

--- kpama/easybuilder/src/Lib/Query.php
+++ kpama/easybuilder/src/Lib/Query.php
@@ -10,40 +10,88 @@ class Query
 {
 
     protected Request $request;
-    protected array $definition;
+    protected array $params = [];
 
-    public function __construct(array $definition,  Request $request = null)
+    public function build(array $definition, array $params = []): Builder
     {
-        $this->request = $request ?? request();
-    }
-    public function build(string $class, Entity $entity): Builder
-    {
-        $query = $class::query();
-        $query = $this->processRequestQuery($query);
+        $query = $definition['class']::query();
+        $query = $this->processRequestQuery($query, $params);
 
         return $query;
     }
 
-    protected function processRequestQuery(Builder $query): Builder
+    protected function processRequestQuery(Builder $query, array $params = []): Builder
     {
-        foreach ($this->request->query() as $name => $param) {
+        foreach ($params as $name => $param) {
             switch ($name) {
                 case 'with':
                     $query = $this->processWith($param, $query);
+                    break;
+                case 'filter':
+                    $query = $this->processFilters($param, $query);
+                    break;
+                case 'field':
+                    $query = $this->processFields($param, $query);
                     break;
             }
         }
         return $query;
     }
 
-    protected function processWith(string $param, Builder $query): Builder
+    protected function processWith(string | array $param, Builder $query): Builder
     {
-        $relationships = explode(',', $param);
+        $relationships =(is_array($param)) ? $param :  explode(',', $param);
 
-        foreach($relationships as $aRelationship) {
+        foreach ($relationships as $aRelationship) {
 
         }
 
+        return $query;
+    }
+
+    protected function processFilters(array $filters, Builder $query): Builder
+    {
+
+        foreach ($filters as $field => $params) {
+            foreach ($params  as $condition => $value) {
+                switch ($condition) {
+                    case 'eq': // equal
+                    case 'is': // equal
+                        $query->where($field, $value);
+                        break;
+                    case 'in': // in
+                        $query->whereIn($field, (is_array($value)) ? $value: explode(',', $value));
+                        break;
+                    case 'sw': // start with
+                        $query->where($field, 'like', "{$value}%");
+                        break;
+                    case 'ew': // end with
+                        $query->where($field, 'like', "%{$value}");
+                        break;
+                    case 'lk': // like
+                        $query->where($field, 'like', "%{$value}%");
+                        break;
+                    case 'g': // greater than
+                        $query->where($field, '>', $value);
+                        break;
+                    case 'ge': // greater than or equal to
+                        $query->where($field, '>=', $value);
+                        break;
+                    case 'l': // less than
+                        $query->where($field, '<', $value);
+                        break;
+                    case 'le': // less than or equal to
+                        $query->where($field, '<=', $value);
+                        break;
+                }
+            }
+        }
+        return $query;
+    }
+
+    protected function processFields($param,  Builder $query): Builder
+    {
+        // @todo implement
         return $query;
     }
 }
