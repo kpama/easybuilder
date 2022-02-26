e973a7b04f16c14d9185ab911bc8a512c1896cc0
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-26 08:26:45 +0000

    Start work on the form definition

--- kpama/easybuilder/src/Lib/Transformer/Form.php
+++ kpama/easybuilder/src/Lib/Transformer/Form.php
@@ -10,8 +10,73 @@ class Form implements TransformerInterface
 
   public function transform(array $parsedData, bool $appendRelationships = true): array
   {
-    $definition = [];
-    
+
+    $definition = [
+      /*[
+        'type' => 'layout',
+        'flow' => 'horizontal', // vertical,
+        'forms' => [],
+        'groups' => []
+      ] */];
+
+    $fields = [];
+    foreach ($parsedData['columns'] as $name => $def) {
+      $currentField = null;
+      switch ($def['type_name']) {
+        case 'bigint':
+        case 'int':
+          $currentField = $this->buildIntField($def, $parsedData);
+          break;
+        case 'string':
+          $currentField = $this->buildStringField($def, $parsedData);
+      }
+
+      if ($currentField) {
+        $fields[$name] = [
+          "in_create" => $def['in_create'],
+          "in_update" => $def['in_update'],
+        ] + $currentField;
+      }
+    }
+
+    $definition[] = [
+      'type' => 'form',
+      'fields' => $fields
+    ];
+
     return  $definition;
+  }
+
+
+  protected function buildIntField(array $def, array $fullDefinition): array
+  {
+    $field = [
+      'type' => 'input',
+      'name' => $def['name'],
+      'value' => 0,
+      'attributes' => [
+        'type' => ($def['in_create'] || $def['in_update']) ? 'number': 'hidden',
+        'step' => 1,
+        'label' => $def['label'],
+      ],
+    ];
+
+    return $field;
+  }
+
+  protected function buildStringField(array $def, array $fullDefinition): array
+  {
+
+    $field = [
+      'type' => 'input',
+      'name' => $def['name'],
+      'value' => '',
+      'attributes' => [
+        'step' => 1,
+        'label' => $def['label'],
+      ],
+    ];
+
+    return $field;
   }
 }
