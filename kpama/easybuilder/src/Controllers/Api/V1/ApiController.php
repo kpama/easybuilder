2ddbfe38c33a37339ce88d2a9b8591b342998772
Jibao Mansaray <jibaomansaray@gmailcom>
2022-02-26 08:16:48 +0000

    Create an instance of the manipulator once

--- kpama/easybuilder/src/Controllers/Api/V1/ApiController.php
+++ kpama/easybuilder/src/Controllers/Api/V1/ApiController.php
@@ -9,38 +9,46 @@ use Kpama\Easybuilder\Lib\Manipulator;
 
 class ApiController extends BaseController implements ApiControllerInterface
 {
+  protected Manipulator $manipulator;
+
+  public function __construct(Manipulator $manipulator)
+  {
+    $this->manipulator = $manipulator;
+  }
+  
   public function index(Request $request, string $resource)
   {
-    return (new Manipulator())->handleGetRequest($request, $resource);
+    return $this->manipulator->handleGetRequest($request, $resource);
   }
 
   public function scope(Request $request, string $scope, string $resource)
   {
+    // @todo use the specified scope as the starting point
     return ['resource' => $resource, 'scope' => $scope];
   }
 
   public function store(Request $request, string $resource)
   {
-    return (new Manipulator())->handleCreateOrUpdateRequest($request, $resource);
+    return $this->manipulator->handleCreateOrUpdateRequest($request, $resource);
   }
 
   public function show(Request $request, mixed $id, string $resource)
   {
-    return (new Manipulator())->handleGetRequest($request, $resource, $id);
+    return $this->manipulator->handleGetRequest($request, $resource, $id);
   }
 
   public function update(Request $request, mixed $id, string $resource)
   {
-    return (new Manipulator())->handleCreateOrUpdateRequest($request, $resource, $id);
+    return $this->manipulator->handleCreateOrUpdateRequest($request, $resource, $id);
   }
 
   public function destroy(mixed $id, string $resource)
   {
-
+    // @todo implement this
   }
 
   public function restore(mixed $id, string $resource)
   {
-
+    // @todo implement this
   }
 }
